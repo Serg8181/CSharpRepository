049d104a5d36de78e3970dcd965f017f952c09eb
Serg8181 <147308625+Serg8181@users.noreply.github.com>
2024-03-18 11:26:41 +0300

    Добавлено задание 5.3.1 и задание 5.5.3

---
+++ Task5.3.1/Task5.3.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        var number = 15;
+
+        Console.WriteLine(number);
+        ChangeAge(ref number);
+        Console.WriteLine(number);
+            
+
+    }
+    static void ChangeAge(ref int  age)
+    {
+        Console.WriteLine("Введите свой возраст: ");
+        age = System.Convert.ToInt32( Console.ReadLine());
+    }
+}
